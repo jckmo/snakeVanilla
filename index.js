c592abe32e898dd0ef7e94084b9e634215a0767a
Jack <jacksonmorgan@hey.com>
2021-05-21 16:46:43 -0400

    build first canvas and show snake

---
+++ index.js
@@ -0,0 +1,15 @@
+const snakeBoard = document.querySelector('#gameCanvas')
+
+const snakeBoardCtx = gameCanvas.getContext('2d')
+
+let snake = [{x: 200, y: 200}, {x: 190, y: 200}, {x: 180, y: 200}, {x: 170, y: 200}, {x: 160, y: 200}]
+
+const drawSnakePart = snakePart => {
+  snakeBoardCtx.fillStyle = 'lightblue'
+  snakeBoardCtx.strokestyle = 'darkblue'
+  snakeBoardCtx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+const drawSnake = () => {
+  snake.forEach(drawSnakePart)
+}
